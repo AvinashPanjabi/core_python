5a9157b5b6ef8a9b172271463de7d4788c055e27
unknown <you@example.com>
2025-01-27 12:11:59 +0530

    python

---
+++ exercises/5 random numbers in 1 to 100.py
@@ -0,0 +1,7 @@
+import random
+print("1st random numbers :",random.randrange(1,101))
+print("2nd random numbers :",random.randrange(1,101))
+print("3rd random numbers :",random.randrange(1,101))
+print("4th random numbers :",random.randrange(1,101))
+print("5th random numbers :",random.randrange(1,101))
+
---
+++ exercises/smaller of two numbers.py
@@ -0,0 +1,6 @@
+a = 10
+b = 20
+if (a<b):
+    print("smallest number is a =",a)
+else:
+    print("smallest number is b =",b)
