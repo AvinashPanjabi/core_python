5fd8eb8c75788355ca33bbdf32605cca3cd88315
unknown <you@example.com>
2025-01-27 11:00:32 +0530

    python

---
+++ control statements/basics.py
@@ -0,0 +1,51 @@
+# if else
+from queue import PriorityQueue
+
+# price = 500000
+# money = 200000
+# if( money > price):
+#     print("i can buy a car")
+# else:
+#     print("i can't buy a car")
+
+
+# my_money = 400
+# if(my_money > 500):
+#     print("i can buy a pizza")
+# else:
+#     print("i can't buy a pizza")
+
+#LOOPS
+
+# for i in range(0,5):
+#     print(i)
+
+# for n in range(2,11):
+#     for i in range(1,11):
+#         print(n,'x',i,'=',n*i)
+
+# string_name = "helloworld"
+# for element in string_name:
+#     print(element)
+
+#while loop
+# i = 0
+# while i<10:
+#     print('hello',i)
+#     i += 2
+
+#BREAK STATEMENT
+
+# str = 'python'
+# for i in str:
+#     if i == 'o':
+#         break
+#     print(i)
+
+#CONTINUE STATEMENT
+i = 1;
+for i in range(1,11):
+    if i==5:
+        continue
+    print("%d"%i)
+
