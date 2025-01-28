b87c0dad67751228e894bb84a8b7a32bc24a8db2
unknown <you@example.com>
2025-01-28 11:33:43 +0530

    done

---
+++ exercises/date and time.py
@@ -0,0 +1,43 @@
+import datetime
+# x = datetime.datetime.now()
+# print(x)
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x.year)
+# print(x.strftime("%A"))
+
+# import datetime
+# x = datetime.datetime(2020,8,3)
+# print(x)
+
+# import datetime
+x = datetime.datetime(2025,1,28)
+print(x.strftime("%a"))
+print(x.strftime("%A"))
+print(x.strftime("%w"))
+print(x.strftime("%d"))
+print(x.strftime("%b"))
+print(x.strftime("%B"))
+print(x.strftime("%m"))
+print(x.strftime("%y"))
+print(x.strftime("%Y"))
+print(x.strftime("%H"))
+print(x.strftime("%I"))
+print(x.strftime("%p"))
+print(x.strftime("%M"))
+print(x.strftime("%S"))
+print(x.strftime("%f"))
+print(x.strftime("%z"))
+print(x.strftime("%Z"))
+print(x.strftime("%j"))
+print(x.strftime("%U"))
+print(x.strftime("%W"))
+print(x.strftime("%c"))
+print(x.strftime("%x"))
+print(x.strftime("%X"))
+print(x.strftime("%%"))
+print(x.strftime("%G"))
+print(x.strftime("%u"))
+print(x.strftime("%V"))
+print(x.strftime("%:z"))
---
+++ exercises/reverse of a number.py
@@ -0,0 +1,6 @@
+# a = 1234
+# for a in (1,5):
+#     print(reversed_number(a))
+
+import sys
+print(sys.version)
